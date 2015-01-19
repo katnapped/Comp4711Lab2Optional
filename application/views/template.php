b0a9c78c0f76cc47bbb0387b34a511d5b9a2590e
Yu-Hsin Sean Wang <katnapped@hotmail.com>
2015-01-18 16:51:58 -0800

    Finaly Copy

---
+++ application/views/template.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<!-- Website Template by freewebsitetemplates.com -->
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Contact - ZiZurz Website Template</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+	<div id="header">
+		<div class="section">
+			<div class="logo">
+				<a href="index.html">zizurz</a>
+			</div>
+			<ul>
+				<li class="selected">
+					<a href="/">home</a>
+				</li>
+				<li>
+					<a href="/about">about</a>
+				</li>
+				<li>
+					<a href="/hairstyle">hairstyles</a>
+				</li>
+				<li>
+					<a href="/news">news</a>
+				</li>
+				<li>
+					<a href="/contact">contact</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div id="body">
+		{content}
+	</div>
+	<div id="footer">
+		<div>
+			<div class="connect">
+				<a href="http://freewebsitetemplates.com/go/twitter/" id="twitter">twitter</a>
+				<a href="http://freewebsitetemplates.com/go/facebook/" id="facebook">facebook</a>
+				<a href="http://freewebsitetemplates.com/go/googleplus/" id="googleplus">googleplus</a>
+				<a href="http://pinterest.com/fwtemplates/" id="pinterest">pinterest</a>
+			</div>
+			<p>
+				&copy; copyright 2023 | all rights reserved.
+			</p>
+		</div>
+	</div>
+</body>
+</html>
